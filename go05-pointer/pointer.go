3a98a03b3c1de5563ef4a1a7a2251716e8230aa3
jho-yf <xu-jihong@qq.com>
2023-03-21 08:01:28 +0800

    feat: 指针

---
+++ go05-pointer/pointer.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func changeValue(p *int) {
+	*p = 10
+}
+
+func swap(pNum1 *int, pNum2 *int) {
+	var temp int = *pNum1
+	*pNum1 = *pNum2
+	*pNum2 = temp
+}
+
+func main() {
+	var a int = 1
+	changeValue(&a)
+	fmt.Println("a = ", a)
+
+	// swap
+	var num1 int = 10
+	var num2 int = 20
+
+	fmt.Println("num1 = ", num1, "num2 = ", num2)
+
+	swap(&num1, &num2)
+
+	fmt.Println("num1 = ", num1, "num2 = ", num2)
+
+	// 定义指针
+	var p *int = &num1
+	// 打印指针地址
+	fmt.Println(&num1)
+	fmt.Println(p)
+
+	// 二级指针
+	var pp **int = &p
+	fmt.Println(&p)
+	fmt.Println(pp)
+}

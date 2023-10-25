9af8a0cf7d67b7bfc9590748c805b98b599caf2b
jho-yf <xu-jihong@qq.com>
2023-10-26 02:24:24 +0800

    feat: 完善指针示例

--- go05-pointer/pointer.go
+++ go05-pointer/pointer.go
@@ -32,9 +32,14 @@ func main() {
 	// 打印指针地址
 	fmt.Println(&num1)
 	fmt.Println(p)
+	fmt.Println(p == &num1)
 
 	// 二级指针
 	var pp **int = &p
 	fmt.Println(&p)
 	fmt.Println(pp)
+	fmt.Println(&p == pp)
+
+	// 三级指针
+	fmt.Println(&pp)
 }

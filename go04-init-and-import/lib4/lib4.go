f1b2e352ebf14c1cabef7cab153473c3cae7903f
jho-yf <xu-jihong@qq.com>
2023-10-26 02:13:24 +0800

    feat: 完善点导入示例

---
+++ go04-init-and-import/lib4/lib4.go
@@ -0,0 +1,11 @@
+package lib3
+
+import "fmt"
+
+func init() {
+	fmt.Println("lib4 init...")
+}
+
+func FuncOfLib4() {
+	fmt.Println("lib4 func...")
+}
--- go04-init-and-import/main.go
+++ go04-init-and-import/main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-	. "fmt" // 将fmt包中所有方法导入当前包中。不推荐使用
-	"yf-go/go04-init-and-import/lib1"
+	"fmt"
+	"yf-go/go04-init-and-import/lib1"        // 如果包被导入，但没有使用，编译会报错
 	myLib2 "yf-go/go04-init-and-import/lib2" // 添加别名
 	_ "yf-go/go04-init-and-import/lib3"      // 匿名导包：不被调用，编译时也不会报错，但会调用init方法
+	. "yf-go/go04-init-and-import/lib4"      // 将fmt包中所有方法导入当前包中，调用的时候可以不加包名。不推荐使用
 )
 
 func main() {
-	Println("main func...")
+	fmt.Println("main func...")
 	lib1.Func()
 	myLib2.Func()
+	FuncOfLib4()
 }
 
 func init() {
-	Println("main init...")
+	fmt.Println("main init...")
 }
